5252646cc230cbe2b5b7414f6192c2689b528264
unknown <you@example.com>
2026-03-10 10:06:39 +0100

    samochodKropkaJava

--- app/src/main/java/com/example/bazadanychsamochody/Samochod.java
+++ app/src/main/java/com/example/bazadanychsamochody/Samochod.java
@@ -1,4 +1,77 @@
 package com.example.bazadanychsamochody;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "tokareczki")
+
 public class Samochod {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String marka;
+    private String model;
+    @ColumnInfo(name = "rocznik")
+    private int rocznik;
+    @ColumnInfo(name = "moc_silnika")
+    private int mocSilnika;
+
+    public Samochod(String marka, String model, int rocznik, int mocSilnika) {
+        this.id = 0;
+        this.marka = marka;
+        this.model = model;
+        this.rocznik = rocznik;
+        this.mocSilnika = mocSilnika;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMarka() {
+        return marka;
+    }
+
+    public void setMarka(String marka) {
+        this.marka = marka;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getRocznik() {
+        return rocznik;
+    }
+
+    public void setRocznik(int rocznik) {
+        this.rocznik = rocznik;
+    }
+
+    public int getMocSilnika() {
+        return mocSilnika;
+    }
+
+    public void setMocSilnika(int mocSilnika) {
+        this.mocSilnika = mocSilnika;
+    }
+
+    @Override
+    public String toString() {
+        return "Tokarka{" +
+                "id=" + id +
+                ", marka='" + marka + '\'' +
+                ", model='" + model + '\'' +
+                ", rocznik=" + rocznik +
+                ", mocSilnika=" + mocSilnika +
+                '}';
+    }
 }
